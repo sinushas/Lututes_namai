dc72ecbf9e29ef39d4e023c25feaccc60d8c048b
sinushas <saulius.sinusas@sbl.lt>
2023-11-30 14:43:37 +0200

    Django start project

---
+++ lututes_namai/lututes_namai/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lututes_namai project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lututes_namai.settings')
+
+application = get_wsgi_application()
